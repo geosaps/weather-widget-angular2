2c66964436566efd5cc1a96f6a8e22fc4868eaac
Pavlo Saps <emandsaps@gmail.com>
2017-09-13 20:34:27 +0300

    First commit, Gettin data from the server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// let apiUrl = 'http://api.wunderground.com/api/';
+// let apikey = '3af5f670f8497198';
+
 export class AppComponent {
-  title = 'app works!';
+	apiUrl: string = 'http://api.wunderground.com/api/';
+	apikey: string = '3af5f670f8497198';
+	data: Object;
+	city: string = 'Tlumach'
+
+	constructor(private http: Http) {
+
+	}
+
+  search(value: string): void {
+  	this.city = value;
+  	const params: string = `${this.apiUrl}${this.apikey}/conditions/q/${this.city}`;
+  	const queryUrl = `${params}.json`;
+  	console.log(queryUrl);
+  	this.http.get(queryUrl)
+      .subscribe((res: Response) => {
+          this.data = res.json().current_observation;
+          console.log(this.data);
+      });
+  }
+
 }
